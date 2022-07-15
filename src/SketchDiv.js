dc36ed0535992a119f8bd02322b88947e16b53a7
Jack <jack.morgan.edwards@gmail.com>
2022-07-15 13:22:30 -0400

    removed slider

--- src/SketchDiv.js
+++ src/SketchDiv.js
@@ -6,7 +6,7 @@ function SketchDiv() {
   let tones = [];
   let poleValues = [55, 110, 220, 440, 880, 1760];
   let poles = [];
-  let massSlider;
+  // let massSlider;
   //set gravity (good at 1 or 2)
   const G = 2;
 
@@ -21,15 +21,16 @@ function SketchDiv() {
       x += 1;
     }
 
-    massSlider = p5.createSlider(11, 150, 10);
-    massSlider.position(10, 100);
-    massSlider.style("width", "80px");
+    // massSlider = p5.createSlider(11, 150, 10);
+    // massSlider.position(10, 100);
+    // massSlider.style("width", "80px");
 
     cnv.mousePressed(() => {
       if (p5.mouseX > p5.width || p5.mouseY > p5.height || p5.mouseY < 0) {
         return;
       }
-      let tone = new ToneCircle(p5, p5.mouseY, massSlider.value(), G);
+      // let tone = new ToneCircle(p5, p5.mouseY, massSlider.value(), G);
+      let tone = new ToneCircle(p5, p5.mouseY, 11, G);
 
       for (let i = tones.length - 1; i >= 0; i--) {
         let dist = tones[i].pos.y - p5.mouseY;
