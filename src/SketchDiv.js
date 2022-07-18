6d3c3948a7348ec2dde472a93cc533fca49dfe95
Jack <jack.morgan.edwards@gmail.com>
2022-07-18 18:50:18 -0400

    comments

--- src/SketchDiv.js
+++ src/SketchDiv.js
@@ -16,7 +16,7 @@ function SketchDiv({ distortion, reverb }) {
     let x = 1;
     let cnv = p5.createCanvas(800, 1600).parent(canvasParentRef);
     cnv.position(25, 120);
-
+    //instantiate all poles in poleValues
     for (let q = poleValues.length - 1; q >= 0; q--) {
       let pole = new Pole(p5, poleValues[q], x * x, G);
       setPoles((prevState) => [...prevState, pole]);
@@ -27,11 +27,11 @@ function SketchDiv({ distortion, reverb }) {
     // massSlider.position(10, 100);
     // massSlider.style("width", "80px");
 
+    //create new tone and add to list
     cnv.mousePressed(() => {
       if (p5.mouseX > p5.width || p5.mouseY > p5.height || p5.mouseY < 0) {
         return;
       }
-      // let tone = new ToneCircle(p5, p5.mouseY, massSlider.value(), G);
       let tone = new ToneCircle(p5, p5.mouseY, 11, reverb, distortion);
 
       for (let i = tones.length - 1; i >= 0; i--) {
@@ -44,7 +44,7 @@ function SketchDiv({ distortion, reverb }) {
         force.setMag(strength);
         tones[i].applyForce(force);
       }
-
+      //add tone to current list
       setTones((prevState) => {
         let newTones = [...prevState];
         newTones.push(tone);
@@ -56,9 +56,11 @@ function SketchDiv({ distortion, reverb }) {
   const draw = (p5) => {
     p5.background(0);
     let allPos = {};
+    //display all poles
     for (let q = poles.length - 1; q >= 0; q--) {
       poles[q].display();
     }
+    //update info for all tones current displayed
     for (let i = tones.length - 1; i >= 0; i--) {
       tones[i].recordHistory();
       tones[i].updateHistory();
@@ -71,6 +73,7 @@ function SketchDiv({ distortion, reverb }) {
       tones[i].display();
       tones[i].play();
       tones[i].loseMass();
+      //check that each tone is above threshold, otherwise remove
       if (tones[i].checkMass()) {
         tones[i].mass = 0;
         for (let i = tones.length - 1; i >= 0; i--) {
@@ -81,10 +84,10 @@ function SketchDiv({ distortion, reverb }) {
         return;
       }
     }
+    //record all tone positions in dictionary
     for (let i = tones.length - 1; i >= 0; i--) {
       allPos[i] = [tones[i].pos.y, tones[i].mass];
     }
-    // console.log(allPos);
   };
 
   function resetAllIndices() {
